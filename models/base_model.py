8542a6defea8340d722209ab4c327cd3c489dcee
mouad sodor <sodormouad@gmail.com>
2024-03-10 20:41:35 +0000

    edit base model

--- models/base_model.py
+++ models/base_model.py
@@ -6,19 +6,23 @@ import uuid
 from datetime import datetime
 import models
 
-class BaseModel:
-    """ this is the base class for all my classes"""
     def __init__(self, *args, **kwargs):
-        """the BaseModel constructor"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key != "__class__":
-                    setattr(self, key, value)
+        """Initialize a new BaseModel.
+
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
+                else:
+                    self.__dict__[k] = v
         else:
             models.storage.new(self)
 
