2e82a8440bbe5fd08976bbf8b896c70cb1f318da
mouad sodor <sodormouad@gmail.com>
2024-03-10 20:45:21 +0000

    edit __str__

--- models/base_model.py
+++ models/base_model.py
@@ -24,7 +24,7 @@ class BaseModel:
 
     def __str__(self):
         """returns the string repr of the object"""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """saves the object to the json filee"""
@@ -34,7 +34,7 @@ class BaseModel:
     def to_dict(self):
         """converts the object to a dictionary"""
         new_dict = self.__dict__.copy()
+        new_dict["__class__"] = self.__class__.__name__
         new_dict["created_at"] = self.created_at.isoformat()
         new_dict["updated_at"] = self.updated_at.isoformat()
-        new_dict["__class__"] = self.__class__.__name__
         return new_dict
