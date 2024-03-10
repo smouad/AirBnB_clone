7fe606d168043fd52fd058e273cad4bba58a2948
mouad sodor <sodormouad@gmail.com>
2024-03-10 20:38:59 +0000

    edit base model

--- models/base_model.py
+++ models/base_model.py
@@ -6,29 +6,26 @@ import uuid
 from datetime import datetime
 import models
 
+class BaseModel:
     """ this is the base class for all my classes"""
-
     def __init__(self, *args, **kwargs):
         """the BaseModel constructor"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
-                    setattr(self, key,
-                            datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
-                elif key == "__class__":
-                    pass
-                else:
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
                     setattr(self, key, value)
         else:
-            self.id = str(uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
             models.storage.new(self)
 
     def __str__(self):
         """returns the string repr of the object"""
         return "[{}] ({}) {}".format(self.__class__.__name__,
-                                     self.id, self.__dict__)
+                                    self.id, self.__dict__)
 
     def save(self):
         """saves the object to the json filee"""
