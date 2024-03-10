af3f7193f31c6de7a1bb9ebe0dd454e7bada1659
mouad sodor <sodormouad@gmail.com>
2024-03-10 20:37:22 +0000

    push base model

--- models/base_model.py
+++ models/base_model.py
@@ -6,20 +6,23 @@ import uuid
 from datetime import datetime
 import models
 
-class BaseModel:
     """ this is the base class for all my classes"""
+
     def __init__(self, *args, **kwargs):
         """the BaseModel constructor"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key != "__class__":
+                    setattr(self, key,
+                            datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key == "__class__":
+                    pass
+                else:
                     setattr(self, key, value)
         else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             models.storage.new(self)
 
     def __str__(self):
